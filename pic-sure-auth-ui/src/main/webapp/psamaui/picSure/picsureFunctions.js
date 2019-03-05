d204f9755e09cf4b6932f300b80dea8104068d86
Alex Nikitin <aleksandr.nikitin@childrens.harvard.edu>
2019-03-05 14:06:12 -0500

    Connection related fixes (adding users, managing connections);

--- pic-sure-auth-ui/src/main/webapp/psamaui/picSure/picsureFunctions.js
+++ pic-sure-auth-ui/src/main/webapp/psamaui/picSure/picsureFunctions.js
@@ -6,10 +6,13 @@ define(["util/notification", "picSure/settings"],
     };
 
     var connections = undefined;
-    
-    picsureFunctions.getConnection = function (connectionUuid, callback) {
-        if(connections){
-            callback(_.findWhere(connections, {uuid: connectionUuid}));
+    // forceAjax flag is to decide where get connections from API or local storage
+    picsureFunctions.getConnection = function (connectionUuid, forceAjax, callback) {
+        if(!forceAjax && connections) {
+            if (connectionUuid)
+                callback(_.findWhere(connections, {uuid: connectionUuid}));
+            else
+                callback(connections);
         }else{
             $.ajax({
                 url: window.location.origin + settings.basePath + '/connection/' + (connectionUuid ? connectionUuid : ''),
@@ -25,24 +28,6 @@ define(["util/notification", "picSure/settings"],
                 }
             });
         }
-    }.bind(picsureFunctions);
-    picsureFunctions.getConnections = function (callback) {
-    	    if(connections){
-    	    		callback(connections);
-    	    }else{
-    	        $.ajax({
-    	            url: window.location.origin + settings.basePath + '/connection/',
-    	            type: 'GET',
-    	            contentType: 'application/json',
-    	            success: function(response){
-    	            		connections = response;
-    	                callback(response);
-    	            },
-    	            error: function(response){
-    	                console.log("Failed to get connections from server.");
-    	            }
-    	        });
-    	    }
     }.bind(picsureFunctions);
     picsureFunctions.createOrUpdateConnection = function (connections, requestType, callback) {
         var successMessage = 'Successfully added a connection.';
