b71a1d58ce9f0b639471bbc53529b609b7b0f554
Ryan Amari <ramari16@gmail.com>
2021-04-06 12:24:14 -0400

    ALS-1932: Revert test changes

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/auth/FENCEAuthenticationService.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/auth/FENCEAuthenticationService.java
@@ -242,9 +242,6 @@ public class FENCEAuthenticationService {
                 //logger.debug("getFENCEProfile() object:"+role_object.toString());
         }
 
-        // todo: REMOVE, this is for testing
-        current_user.getRoles().clear();
-
         final String idp = extractIdp(current_user);
 
         if (current_user.getRoles() != null && (current_user.getRoles().size() > 0 || openAccessIdpValues.contains(idp))) {
