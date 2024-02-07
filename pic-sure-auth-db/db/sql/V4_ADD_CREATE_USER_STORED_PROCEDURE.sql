0aba935ff5b6dcee4d687ed11b471fca85164470
GeorgeC <gcolon021@gmail.com>
2024-02-07 14:26:13 -0500

    Update CreateUserWithRole stored procedure in auth DB
    The stored procedure, CreateUserWithRole, in the 'auth' database has been updated to improve user creation. It now checks for existing users and roles, and generates a new UUID if needed. Additionally, it associates new users with roles if they exist.

--- pic-sure-auth-db/db/sql/V4_ADD_CREATE_USER_STORED_PROCEDURE.sql
+++ pic-sure-auth-db/db/sql/V4_ADD_CREATE_USER_STORED_PROCEDURE.sql
@@ -1,22 +1,31 @@
 USE `auth`;
 
-delimiter //
+DROP PROCEDURE IF EXISTS CreateUserWithRole;
+DELIMITER //
 CREATE PROCEDURE CreateUserWithRole (
     IN user_email VARCHAR(255),
     IN connection_id VARCHAR(255),
     IN role_name VARCHAR(255)
 )
 BEGIN
+    -- Attempt to retrieve the UUIDs for the user and role based on the provided information
 SELECT @userUUID := uuid FROM auth.user WHERE email = user_email AND connectionId = connection_id;
 SELECT @roleUUID := uuid FROM auth.role WHERE name = role_name;
+
+-- If the user does not exist, create a new user entry
 IF @userUUID IS NULL THEN
+        -- Generate a new UUID for the user
         SET @userUUID = UNHEX(REPLACE(UUID(), '-', ''));
+        -- Retrieve the UUID for the connection
 SELECT @connectionUUID := uuid FROM auth.connection WHERE id = connection_id;
-INSERT INTO auth.user (uuid, general_metadata, acceptedTOS, connectionId, email, matched, subject, is_active, long_term_token, isGateAnyRelation)
-VALUES (@userUUID, null, (SELECT CURRENT_TIMESTAMP), @connectionUUID, user_email, 0, null, 1, null, 1);
+-- Insert the new user record into the user table
+INSERT INTO auth.user (uuid, general_metadata, acceptedTOS, connectionId, email, matched, subject, is_active, long_term_token)
+VALUES (@userUUID, NULL, CURRENT_TIMESTAMP, @connectionUUID, user_email, 0, NULL, 1, NULL);
 END IF;
+
+    -- If the role exists, associate the user with the role
     IF @roleUUID IS NOT NULL THEN
         INSERT INTO auth.user_role (user_id, role_id) VALUES (@userUUID, @roleUUID);
 END IF;
 END//
-delimiter;
+DELIMITER ;
