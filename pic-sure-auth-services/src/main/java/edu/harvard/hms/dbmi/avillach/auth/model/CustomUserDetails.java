1f80323ea178fa92e514e1bda47667ed3c16dcd0
GeorgeC <gcolon021@gmail.com>
2024-04-23 08:22:05 -0400

    Update account status return values in CustomUserDetails
    The code updates the return values for account status checks in the CustomUserDetails class. Previously, these methods returned false regardless of the actual account status. Now, they correctly return true, indicating a non-expired and non-locked account status.

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/model/CustomUserDetails.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/model/CustomUserDetails.java
@@ -34,17 +34,17 @@ public class CustomUserDetails extends User implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
