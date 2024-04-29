9895a127d103e0e351d7570aa353e902d9f1038a
GeorgeC <gcolon021@gmail.com>
2024-04-29 12:33:46 -0400

    Refactor role names in AuthNaming class
    The role names within the AuthRoleNaming class have been refactored to use SecurityRoles enum values instead of hardcoded strings. This change ensures that the role names are consistent and centrally managed across the codebase.

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/AuthNaming.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/AuthNaming.java
@@ -1,5 +1,7 @@
 package edu.harvard.hms.dbmi.avillach.auth.utils;
 
+import edu.harvard.hms.dbmi.avillach.auth.enums.SecurityRoles;
+
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +19,8 @@ public class AuthNaming {
      * <p>Note: Only users with super admin access can edit these role names.</p>
      */
     public static class AuthRoleNaming {
-        public static final String ADMIN = "ADMIN";
-        public static final String SUPER_ADMIN = "SUPER_ADMIN";
+        public static final String ADMIN = SecurityRoles.ADMIN.getRole();
+        public static final String SUPER_ADMIN = SecurityRoles.SUPER_ADMIN.getRole();
 
         public static List<String> allRoles(){
             List<String> roles = new ArrayList<>();
