f862d768dff6e9c31861d18e6974a8c1d2b79ec3
GeorgeC <gcolon021@gmail.com>
2024-04-29 08:58:11 -0400

    Remove orphanRemoval attribute from OneToMany relationship
    The orphanRemoval attribute, initially set to true in the OneToMany relationship between Application and Privilege entities, has been removed. This change means that any Privilege that gets disassociated from an Application will not automatically be deleted from the database.

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/entity/Application.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/entity/Application.java
@@ -22,7 +22,7 @@ public class Application extends BaseEntity implements Principal {
     private String url;
     private boolean enable = true;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "application_id", referencedColumnName = "uuid")
     private Set<Privilege> privileges;
 
