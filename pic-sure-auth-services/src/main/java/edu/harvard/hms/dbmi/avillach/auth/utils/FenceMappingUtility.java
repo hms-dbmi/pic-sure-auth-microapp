e38f878e250aa6e180c5a63663dd3196115d98e2
GeorgeC <gcolon021@gmail.com>
2024-05-13 08:55:30 -0400

    Refactor FenceMappingUtility class
    Refactored the FenceMappingUtility class to initialize the ObjectMapper and templatePath in the init method. Replaced the dependency on JAXRSConfiguration.

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/FenceMappingUtility.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/FenceMappingUtility.java
@@ -1,35 +1,44 @@
 package edu.harvard.hms.dbmi.avillach.auth.utils;
 
-import edu.harvard.hms.dbmi.avillach.auth.JAXRSConfiguration;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.harvard.hms.dbmi.avillach.auth.model.BioDataCatalyst;
 import edu.harvard.hms.dbmi.avillach.auth.model.FenceMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.DependsOn;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 @Singleton
 @Startup
-@DependsOn("JAXRSConfiguration")
 public class FenceMappingUtility {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private Map<String, BioDataCatalyst> _projectMap;
     private Map<String, BioDataCatalyst> fenceMappingByAuthZ;
+    private static String templatePath;
+    private ObjectMapper objectMapper;
 
     @PostConstruct
     public void init() {
         try {
+            Context ctx = new InitialContext();
+            templatePath = (String) ctx.lookup("java:global/templatePath");
+            objectMapper = new ObjectMapper();
+
             initializeFENCEMapping();
             initializeFenceMappingByAuthZ();
         } catch (IOException e) {
             logger.error("Error initializing FENCE mappings", e);
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -64,15 +73,16 @@ public class FenceMappingUtility {
         FenceMapping fenceMapping;
         ArrayList<BioDataCatalyst> projects;
         try {
-            fenceMapping = JAXRSConfiguration.objectMapper.readValue(
+            logger.debug("getFENCEMapping: loading FENCE mapping from {}", templatePath);
+            fenceMapping = objectMapper.readValue(
                     new File(String.join(File.separator,
-                            new String[] {JAXRSConfiguration.templatePath ,"fence_mapping.json"}))
+                            new String[]{templatePath, "fence_mapping.json"}))
                     , FenceMapping.class);
 
             projects = fenceMapping.getProjectMetaData();
             logger.debug("getFENCEMapping: found FENCE mapping with {} entries", projects.size());
         } catch (Exception e) {
-            logger.error("loadFenceMappingData: Non-fatal error parsing fence_mapping.json: {}", JAXRSConfiguration.templatePath, e);
+            logger.error("loadFenceMappingData: Non-fatal error parsing fence_mapping.json: {}", templatePath, e);
             return new ArrayList<>();
         }
         return projects;
