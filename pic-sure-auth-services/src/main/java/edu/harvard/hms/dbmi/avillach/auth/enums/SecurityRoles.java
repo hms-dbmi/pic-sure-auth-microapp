5337420a7248d28a2521ceee8ac079fc84e657bb
GeorgeC <gcolon021@gmail.com>
2024-05-15 14:39:33 -0400

    Removed unnecessary contains methods

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/enums/SecurityRoles.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/enums/SecurityRoles.java
@@ -1,6 +1,5 @@
 package edu.harvard.hms.dbmi.avillach.auth.enums;
 
-import java.util.List;
 import java.util.Set;
 
 public enum SecurityRoles {
@@ -23,52 +22,11 @@ public enum SecurityRoles {
      * @return True if the role is contained in the set of roles
      */
     public static boolean contains(Set<String> roles, String role) {
-        for (String r : roles) {
-            if (r.equals(role)) {
-                return true;
-            }
-        }
-        return false;
+        return roles.contains(role);
     }
 
     public String getRole() {
         return role;
-    }
-
-    public static SecurityRoles getRole(String role) {
-        for (SecurityRoles securityRole : SecurityRoles.values()) {
-            if (securityRole.getRole().equals(role)) {
-                return securityRole;
-            }
-        }
-        return null;
-    }
-
-    public static boolean contains(String role) {
-        for (SecurityRoles securityRole : SecurityRoles.values()) {
-            if (securityRole.getRole().equals(role)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean contains(SecurityRoles role) {
-        for (SecurityRoles securityRole : SecurityRoles.values()) {
-            if (securityRole.equals(role)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean contains(List<String> roles) {
-        for (String role : roles) {
-            if (!contains(role)) {
-                return false;
-            }
-        }
-        return true;
     }
 
 }
