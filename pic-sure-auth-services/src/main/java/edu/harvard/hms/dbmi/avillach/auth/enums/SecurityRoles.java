35de2ffe45f6d800f94990bc4d51712056153eb1
GeorgeC <gcolon021@gmail.com>
2024-05-15 16:02:34 -0400

    Remove contains method

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/enums/SecurityRoles.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/enums/SecurityRoles.java
@@ -1,6 +1,5 @@
 package edu.harvard.hms.dbmi.avillach.auth.enums;
 
-import java.util.Set;
 
 public enum SecurityRoles {
 
@@ -12,17 +11,6 @@ public enum SecurityRoles {
 
     SecurityRoles(String role) {
         this.role = role;
-    }
-
-    /**
-     * Check if a role is contained in a set of roles
-     *
-     * @param roles User roles
-     * @param role Role to check
-     * @return True if the role is contained in the set of roles
-     */
-    public static boolean contains(Set<String> roles, String role) {
-        return roles.contains(role);
     }
 
     public String getRole() {
