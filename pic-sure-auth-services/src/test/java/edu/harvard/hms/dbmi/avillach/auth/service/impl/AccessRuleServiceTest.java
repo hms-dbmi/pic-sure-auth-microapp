59936f8ecedfe9a9571463b473848841af84ae9d
GeorgeC <gcolon021@gmail.com>
2024-06-10 10:51:21 -0400

    Remove duplicate AccessRuleService implementation

--- pic-sure-auth-services/src/test/java/edu/harvard/hms/dbmi/avillach/auth/service/impl/AccessRuleServiceTest.java
+++ pic-sure-auth-services/src/test/java/edu/harvard/hms/dbmi/avillach/auth/service/impl/AccessRuleServiceTest.java
@@ -2,6 +2,7 @@ package edu.harvard.hms.dbmi.avillach.auth.service.impl;
 
 import edu.harvard.hms.dbmi.avillach.auth.entity.AccessRule;
 import edu.harvard.hms.dbmi.avillach.auth.repository.AccessRuleRepository;
+import edu.harvard.hms.dbmi.avillach.auth.service.impl.authorization.AccessRuleService;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
