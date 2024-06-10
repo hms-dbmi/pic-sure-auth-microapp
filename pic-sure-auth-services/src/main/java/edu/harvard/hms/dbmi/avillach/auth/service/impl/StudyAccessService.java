75d06179398c4ef2395354c78f0e64c73ebbefc2
GeorgeC <gcolon021@gmail.com>
2024-06-10 10:30:47 -0400

    Refactor StudyAccessService class and create new StudyAccessController

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/impl/StudyAccessService.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/impl/StudyAccessService.java
@@ -1,35 +1,17 @@
 package edu.harvard.hms.dbmi.avillach.auth.service.impl;
 
-import edu.harvard.hms.dbmi.avillach.auth.model.response.PICSUREResponse;
 import edu.harvard.hms.dbmi.avillach.auth.service.impl.authentication.FENCEAuthenticationService;
 import edu.harvard.hms.dbmi.avillach.auth.utils.FenceMappingUtility;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.Map;
 
-import static edu.harvard.hms.dbmi.avillach.auth.utils.AuthNaming.AuthRoleNaming.SUPER_ADMIN;
-
-/**
- * <p>Endpoint for service handling business logic for adding all the auth
- * rules for a given study</p>
- * <p>Note: Only users with the super admin role can access this endpoint.</p>
- */
-@Tag(name = "Study Access")
-@Controller
-@RequestMapping("/studyAccess")
+@Service
 public class StudyAccessService {
     private final FenceMappingUtility fenceMappingUtility;
     Logger logger = LoggerFactory.getLogger(StudyAccessService.class);
@@ -46,15 +28,10 @@ public class StudyAccessService {
         this.fenceMappingUtility = fenceMappingUtility;
     }
 
-    @Operation(description = "POST a single study and it creates the role, privs, and rules for it, requires SUPER_ADMIN role")
-    @Transactional
-    @PostMapping(consumes = "application/json")
-    @Secured(SUPER_ADMIN)
-    public ResponseEntity<?> addStudyAccess(@Parameter(description="The Study Identifier of the new study from the metadata.json")
+    public String addStudyAccess(@Parameter(description="The Study Identifier of the new study from the metadata.json")
                                                 @RequestBody String studyIdentifier) {
-
         if (StringUtils.isBlank(studyIdentifier)) {
-            return PICSUREResponse.error("Study identifier cannot be blank");
+            return "Error: Study identifier cannot be blank";
         }
 
         Map fenceMappingForStudy;
@@ -67,12 +44,12 @@ public class StudyAccessService {
         } catch(Exception ex) {
             logger.error(ex.toString());
             logger.error("addStudyAccess - Error occurred while fetching FENCE mapping");
-            return PICSUREResponse.error("Error occurred while fetching FENCE mapping");
+            return "Error: occurred while fetching FENCE mapping";
         }
 
         if (fenceMappingForStudy == null || fenceMappingForStudy.isEmpty()) {
             logger.error("addStudyAccess - Could not find study: {} in FENCE mapping", studyIdentifier);
-            return PICSUREResponse.error("Could not find study with the provided identifier");
+            return "Error: Could not find study with the provided identifier";
         }
 
         String projectId = (String) fenceMappingForStudy.get(STUDY_IDENTIFIER);
@@ -83,10 +60,10 @@ public class StudyAccessService {
 
         if (fenceAuthenticationService.upsertRole(null, newRoleName, MANUAL + " role "+newRoleName)) {
             logger.info("addStudyAccess - Updated user role. Now it includes `{}`", newRoleName);
-             return PICSUREResponse.success("Role '" + newRoleName + "' successfully created");
+            return "Error: Role '" + newRoleName + "' successfully created";
         } else {
             logger.error("addStudyAccess - could not add {} role to to database", newRoleName);
-            return PICSUREResponse.error("Could not add role '" + newRoleName + "' to database");
+            return "Could not add role '" + newRoleName + "' to database";
         }
     }
 }
