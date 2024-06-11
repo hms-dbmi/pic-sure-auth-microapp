ced838b550aa0c7d947c672ac4f05cebc8e3e0fd
GeorgeC <gcolon021@gmail.com>
2024-06-11 10:19:56 -0400

    Merge Fence specific branch into release
    ### Build
    - Created `Jenkinsfile.fence`, which is used by Jenkins to build and upload this application to S3.
    
    ### AccessRule
    Merged both `AccessRule` entities.
    - Added two `AccessRule.TypeNaming` constants: `ALL_CONTAINS_OR_EMPTY` and `ALL_CONTAINS_OR_EMPTY_IGNORE_CASE`.
    - Migrated `subAccessRule` and `subAccessRuleParent` to a single `subAccessRule` property that uses a `@JoinTable` to represent the same relationship.
    - Added a new migration script to update existing `AccessRule` tables to use the new `JoinTable`.
    
    ### AccessRuleService
    Merged both `AccessRuleService` classes.
    - Updated all logic to account for the two new `AccessRule.TypeNaming` constants.
    - Refactored the AccessRule cache in the FENCE branch to use Spring's `@Cacheable` and `@CacheEvict`.
    
    ### AuthorizationService
    Merged both `AuthorizationService` classes.
    - Introduced a new property named `strictConnections`, which is assigned a comma-separated list of connection labels. These connections will be evaluated using a more strict authentication flow, requiring the user to have associated access rules and privileges. For non-strict connections, the user only needs associated privileges.
        - This approach may be revisited later. Standardizing on one approach is advisable.
    
    ### FenceAuthenticationService
    - Removed the existing `FenceAuthenticationService` and replaced it with the version from the Fence-specific branch.
    - Incorporated the most recent changes to Role, Privilege, AccessRule, and Fence Mapping into the `FenceAuthenticationService`, including the `FenceMappingUtility` class.
    
    ### Additional Authentication Services
    - Added `Okta0AuthAuthenticationService`, `OpenAuthenticationService`, and `StudyAccessService` to the merged branch. These services previously existed only in the Fence branch.
    
    ### Controllers
    - Refactored all service classes migrated from the Fence-specific branch to have associated `Controller` classes.
    
    ### Java 21 & Spring
    - Refactored all classes migrated from the Fence-specific branch to accommodate Java 21, Spring Boot 3, and Spring 6.
    
    ### Unit Test
    - Merged all unit tests from the Fence branch into related test classes or added them to the project. Ensured that no test changes were logically altered, providing confidence when merging and refactoring classes. The goal was to ensure no currently available test cases failed.

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/RestClientUtil.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/RestClientUtil.java
@@ -77,4 +77,14 @@ public class RestClientUtil {
         }
     }
 
+    public static ResponseEntity<String> retrievePostResponseWithParams(String uri, HttpHeaders headers, String body, Object... params) {
+        try {
+            logger.debug("HttpClientUtilSpring retrievePostResponseWithParams()");
+            HttpEntity<String> entity = new HttpEntity<>(body, headers);
+            return restTemplate.postForEntity(uri, entity, String.class, params);
+        } catch (HttpClientErrorException ex) {
+            logger.error("HttpClientErrorException: {}", ex.getMessage());
+            throw ex;
+        }
+    }
 }
