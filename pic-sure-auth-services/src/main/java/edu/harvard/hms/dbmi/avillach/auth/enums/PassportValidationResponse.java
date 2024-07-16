fa8564a16f4a9256e15ba7a82b061aebc5ed6675
GeorgeC <gcolon021@gmail.com>
2024-07-16 16:08:52 -0400

    Add string values to PassportValidationResponse enum

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/enums/PassportValidationResponse.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/enums/PassportValidationResponse.java
@@ -2,23 +2,34 @@ package edu.harvard.hms.dbmi.avillach.auth.enums;
 
 public enum PassportValidationResponse {
     // The Visa’s signature is valid, the Visa has not expired, a valid txn claim is present
-    VALID,
+    VALID("Valid"),
     // The encoded Visa’s signature failed validation, is missing or incorrect
-    INVALID,
+    INVALID("Invalid"),
     // The Passport payload or Visa parameter is missing
-    MISSING,
+    MISSING("Missing"),
     // The format of the Passport or Visa is incorrect
-    INVALID_PASSPORT,
+    INVALID_PASSPORT("Invalid Passport"),
     // The Visa has expired (as indicated by the “exp” claim)
-    VISA_EXPIRED,
+    VISA_EXPIRED("Visa Expired"),
     // The Transaction claim (txn) has an error or is missing
-    TXN_ERROR,
+    TXN_ERROR("Txn Error"),
     // The Expiration claim (exp) has an error or is missing
-    EXPIRATION_ERROR,
+    EXPIRATION_ERROR("Expiration Error"),
     // A DB related validation error occurred
-    VALIDATION_ERROR,
+    VALIDATION_ERROR("Validation Error"),
     // The Visa was expired because the required polling time was exceeded
-    EXPIRED_POLLING,
+    EXPIRED_POLLING("Expired Polling"),
     // There is a change in the dbGaP permissions associated with the user
-    PERMISSION_UPDATE
+    PERMISSION_UPDATE("Permission Update");
+
+    private final String value;
+
+    PassportValidationResponse(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
 }
