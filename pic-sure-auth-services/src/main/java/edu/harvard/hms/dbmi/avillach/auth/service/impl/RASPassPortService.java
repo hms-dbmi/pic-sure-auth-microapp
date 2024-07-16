cb22d40ef3384b424e6f1605c50b9cbbb91f2676
GeorgeC <gcolon021@gmail.com>
2024-07-16 16:09:28 -0400

    Add passport validation to RASPassPortService
    Added a method to validate passports and used a RestClientUtil to handle the validation communication. Also, a few more new dependencies are imported and necessary parameters are initialized during the object construction phase and updated later as required.

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/impl/RASPassPortService.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/impl/RASPassPortService.java
@@ -1,12 +1,21 @@
 package edu.harvard.hms.dbmi.avillach.auth.service.impl;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.harvard.hms.dbmi.avillach.auth.model.ras.Ga4ghPassportV1;
 import edu.harvard.hms.dbmi.avillach.auth.model.ras.RasDbgapPermission;
 import edu.harvard.hms.dbmi.avillach.auth.utils.JWTUtil;
+import edu.harvard.hms.dbmi.avillach.auth.utils.RestClientUtil;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -16,6 +25,40 @@ import java.util.Set;
 public class RASPassPortService {
 
     private final Logger logger = LoggerFactory.getLogger(RASPassPortService.class);
+    private final RestClientUtil restClientUtil;
+
+    @Value("ras.uri")
+    private String rasURI;
+
+    @Autowired
+    public RASPassPortService(RestClientUtil restClientUtil) {
+        this.restClientUtil = restClientUtil;
+    }
+
+    @PostConstruct
+    public void init() {
+        // remove any trailing / from the rasURI.
+        rasURI = rasURI.replaceAll("/$", "");
+    }
+
+    public Optional<JsonNode> validatePassport(String passport) {
+        // send the passport to the https://stsstg.nih.gov/passport/validate?visa=<the encoded passport>
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+        queryParams.add("visa", passport);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(queryParams);
+
+        JsonNode respJson;
+        ResponseEntity<String> resp;
+        try {
+            resp = this.restClientUtil.retrievePostResponse("/passport/validate", request);
+            respJson = new ObjectMapper().readTree(resp.getBody());
+        } catch (Exception e) {
+            logger.error("Failed to validate passport: {}", passport, e);
+            return Optional.empty();
+        }
+
+        return Optional.ofNullable(respJson);
+    }
 
     public Set<RasDbgapPermission> ga4gpPassportToRasDbgapPermissions(JsonNode introspectResponse) {
         if (introspectResponse == null) {
@@ -37,4 +80,7 @@ public class RASPassPortService {
         return rasDbgapPermissions;
     }
 
+    public void setRasURI(String rasURI) {
+        this.rasURI = rasURI;
+    }
 }
