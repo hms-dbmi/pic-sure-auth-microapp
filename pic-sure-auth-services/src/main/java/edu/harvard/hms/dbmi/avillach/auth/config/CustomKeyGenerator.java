605f541ff042829bb10392891ff6d9025be7ad51
GeorgeC <gcolon021@gmail.com>
2024-10-11 13:50:11 -0400

    Revert CustomKeyGenerator changes

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/config/CustomKeyGenerator.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/config/CustomKeyGenerator.java
@@ -1,32 +1,18 @@
 package edu.harvard.hms.dbmi.avillach.auth.config;
 
 import edu.harvard.hms.dbmi.avillach.auth.entity.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cache.interceptor.KeyGenerator;
 
 import java.lang.reflect.Method;
 
 public class CustomKeyGenerator implements KeyGenerator {
 
-    private final Logger logger = LoggerFactory.getLogger(CustomKeyGenerator.class);
-
     @Override
     public Object generate(Object target, Method method, Object... params) {
-        String key = null;
         for (Object param : params) {
             if (param instanceof User user) {
-                key = user.getSubject();
+                return user.getSubject();
             }
-
-            if (param instanceof String userSubject) {
-                key = userSubject;
-            }
-        }
-
-        if (key != null) {
-            logger.info("Generated cache key: {}", key);
-            return key;
         }
 
         throw new IllegalArgumentException("No valid params found. Cannot generate cache key");
