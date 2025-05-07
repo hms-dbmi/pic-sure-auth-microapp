941dfe06552469a0bbf940d1f7f09ddf03830bf5
GeorgeC <gcolon021@gmail.com>
2025-05-07 14:30:57 -0400

    Revert EAGER fetch

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/entity/Privilege.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/entity/Privilege.java
@@ -24,7 +24,7 @@ public class Privilege extends BaseEntity {
     @JoinColumn(name = "application_id")
     private Application application;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "accessRule_privilege",
             joinColumns = {@JoinColumn(name = "privilege_id")},
             inverseJoinColumns = {@JoinColumn(name = "accessRule_id")})
