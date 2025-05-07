e97462b4604b91f793e3d4660a2575eb37bf7e45
GeorgeC <gcolon021@gmail.com>
2025-05-07 13:35:08 -0400

    Refactor role-based authority handling in CustomUserDetails
    Updated the constructor to properly map user roles and dynamically add top admin privileges when applicable. Introduced new imports for improved role management and logging for debugging purposes. These changes enhance clarity and ensure more robust role-based access control.

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/model/CustomUserDetails.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/model/CustomUserDetails.java
@@ -1,23 +1,37 @@
 package edu.harvard.hms.dbmi.avillach.auth.model;
 
 import edu.harvard.hms.dbmi.avillach.auth.entity.User;
+import edu.harvard.hms.dbmi.avillach.auth.enums.SecurityRoles;
+import edu.harvard.hms.dbmi.avillach.auth.utils.AuthNaming;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class CustomUserDetails implements UserDetails {
 
+    private final static Logger log = Logger.getLogger(CustomUserDetails.class.getName());
     private final User user;
     private final Collection<? extends GrantedAuthority> authorities;
 
     public CustomUserDetails(User user) {
         this.user = user;
         if (user != null && user.getRoles() != null) {
-            this.authorities = user.getTotalPrivilege().stream()
-                    .map(privilege -> (GrantedAuthority) privilege::getName)
-                    .toList();
+            List<GrantedAuthority> authorities = new ArrayList<>(user.getRoles().stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .toList());
+
+            if (authorities.stream().anyMatch(role -> role.getAuthority().equals(SecurityRoles.PIC_SURE_TOP_ADMIN.getRole()))) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_"+AuthNaming.AuthRoleNaming.ADMIN));
+                authorities.add(new SimpleGrantedAuthority("ROLE_"+AuthNaming.AuthRoleNaming.SUPER_ADMIN));
+            }
+
+            this.authorities = authorities;
         } else {
             this.authorities = new ArrayList<>();
         }
