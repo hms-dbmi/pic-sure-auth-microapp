2aae94150c9863829962486947f90d2b67ffc2bc
Ryan Amari <ramari16@gmail.com>
2026-02-12 19:24:00 +0100

    ALS-9545.2: Fix NPE

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/impl/authorization/BdcConsentsBuilder.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/impl/authorization/BdcConsentsBuilder.java
@@ -2,6 +2,8 @@ package edu.harvard.hms.dbmi.avillach.auth.service.impl.authorization;
 
 import edu.harvard.hms.dbmi.avillach.auth.model.fenceMapping.StudyMetaData;
 import edu.harvard.hms.dbmi.avillach.auth.model.ras.RasDbgapPermission;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -9,7 +11,10 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+
 public class BdcConsentsBuilder {
+
+    private final Logger log = LoggerFactory.getLogger(BdcConsentsBuilder.class);
 
     public static final String CONSENTS_KEY = "_consents";
     public static final String HARMONIZED_CONSENTS_KEY = "_harmonized_consents";
@@ -31,9 +36,13 @@ public class BdcConsentsBuilder {
         Map<String, Set<String>> result = new HashMap<>();
 
         userConsentStrings.forEach(consent -> {
+            StudyMetaData studyMetaData = fenceMappingByConsent.get(consent);
+            if (studyMetaData == null) {
+                log.info(consent + " not found in fence mapping");
+                return;
+            }
             result.computeIfAbsent(CONSENTS_KEY, _ -> new HashSet<>()).add(consent);
 
-            StudyMetaData studyMetaData = fenceMappingByConsent.get(consent);
             if (studyMetaData.getIsHarmonized()) {
                 Set<String> harmonizedConsents = result.getOrDefault(HARMONIZED_CONSENTS_KEY, new HashSet<>());
                 harmonizedConsents.add(consent);
