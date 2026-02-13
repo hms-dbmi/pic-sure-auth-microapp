a1a922a425c354591e329dee07f63595b31676e8
Ryan Amari <ramari16@gmail.com>
2026-02-13 21:09:01 +0100

    ALS-9545: Add todo

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/impl/authorization/AuthorizationService.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/impl/authorization/AuthorizationService.java
@@ -223,10 +223,7 @@ public class AuthorizationService {
                         result = true;
                         passByRule = accessRule;
 
-                        List<AuthorizationFilter> authorizationFilter = userConsents.getConsents().entrySet().stream()
-                                .map(entry -> new AuthorizationFilter(entry.getKey(), entry.getValue())).toList();
-
-                        returnQuery = query.setAuthorizationFilters(authorizationFilter);
+                        returnQuery = setAuthorizationFiltersForQuery(userConsents, query);
                         break;
                     } else {
                         failedRules.add(accessRule);
@@ -265,6 +262,18 @@ public class AuthorizationService {
         }
 
         return new EvaluateAccessRuleResult(result, failedRules, passRuleName, Optional.ofNullable(returnQuery));
+    }
+
+    private Query setAuthorizationFiltersForQuery(UserConsents userConsents, Query query) {
+        List<AuthorizationFilter> authorizationFilter = userConsents.getConsents().entrySet().stream()
+                .filter(entry -> {
+                    // todo: add logic to remove topmed and harmonized consents when not applicable
+                    return true;
+                })
+                .map(entry -> new AuthorizationFilter(entry.getKey(), entry.getValue())).toList();
+
+
+        return query.setAuthorizationFilters(authorizationFilter);
     }
 
 
