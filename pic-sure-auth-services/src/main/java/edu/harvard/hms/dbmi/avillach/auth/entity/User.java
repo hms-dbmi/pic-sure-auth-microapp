ae3451327a571eef0347854d959e7ccd57232e22
GeorgeC <gcolon021@gmail.com>
2026-06-04 14:44:16 -0400

    Replace null return with empty Set in User entity's privilege handling logic

--- pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/entity/User.java
+++ pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/entity/User.java
@@ -158,7 +158,7 @@ public class User extends BaseEntity implements Serializable, Principal {
 		Set<Privilege> totalPrivilegeSet = getTotalPrivilege();
 
 		if (totalPrivilegeSet == null)
-			return null;
+			return Set.of();
 
 		Set<String> nameSet = new HashSet<>();
 		if (application == null)
